622f0c646121e7fd6287855b31e2ef1ff73848ba
홍혁준 <jjj8337721@naver.com>
2022-11-02 14:35:56 +0900

    init

---
+++ src/test/java/kr/co/popoolserver/PopoolServerApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.popoolserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PopoolServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
