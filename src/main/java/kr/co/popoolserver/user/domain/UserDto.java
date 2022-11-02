35eef3644fb20fb3bcdd41e06c41b2411d3a5fd7
홍혁준 <jjj8337721@naver.com>
2022-11-02 17:23:11 +0900

    feat: User 생성 1. UserEntity 2. User package 설계

---
+++ src/main/java/kr/co/popoolserver/user/domain/UserDto.java
@@ -0,0 +1,4 @@
+package kr.co.popoolserver.user.domain;
+
+public class UserDto {
+}
---
+++ src/main/java/kr/co/popoolserver/user/domain/UserEntity.java
@@ -0,0 +1,75 @@
+package kr.co.popoolserver.user.domain;
+
+import kr.co.popoolserver.common.domain.Address;
+import kr.co.popoolserver.common.domain.BaseEntity;
+import kr.co.popoolserver.common.domain.PhoneNumber;
+import kr.co.popoolserver.common.domain.enums.Gender;
+import kr.co.popoolserver.common.domain.enums.UserRole;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "tbl_user")
+@Getter
+@AttributeOverride(name = "id", column = @Column(name = "user_id"))
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class UserEntity extends BaseEntity {
+
+    @Column(name = "identity", unique = true, nullable = false, length = 25)
+    private String identity;
+
+    @Column(name = "password", unique = true, nullable = false, length = 100)
+    private String password;
+
+    @Column(name = "email", unique = true, length = 50)
+    private String email;
+
+    @Column(name = "name", nullable = false, length = 25)
+    private String name;
+
+    @Column(name = "birth", nullable = false, length = 8)
+    private String birth;
+
+    @Column(name = "gender")
+    @Enumerated(value = EnumType.STRING)
+    private Gender gender;
+
+    @Column(name = "user_role")
+    @Enumerated(value = EnumType.STRING)
+    private UserRole userRole;
+
+    @Embedded
+    @AttributeOverride(name = "phoneNumber", column = @Column(name = "phone_number", unique = true))
+    private PhoneNumber phoneNumber;
+
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name = "zipcode", column = @Column(name = "zipcode")),
+            @AttributeOverride(name = "address1", column = @Column(name = "address1")),
+            @AttributeOverride(name = "address2", column = @Column(name = "address2"))
+    })
+    private Address address;
+
+    @Builder
+    public UserEntity(String identity,
+                      String password,
+                      String email,
+                      String name,
+                      String birth,
+                      Gender gender,
+                      PhoneNumber phoneNumber,
+                      Address address) {
+        this.identity = identity;
+        this.password = password;
+        this.email = email;
+        this.name = name;
+        this.birth = birth;
+        this.gender = gender;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+    }
+}
