b768ad50314b492e50b0ed2ae74aae282e391496
홍혁준 <jjj8337721@naver.com>
2022-11-02 20:41:49 +0900

    fix : password unique 실수 삭제

--- src/main/java/kr/co/popoolserver/corporate/domain/CorporateEntity.java
+++ src/main/java/kr/co/popoolserver/corporate/domain/CorporateEntity.java
@@ -35,7 +35,7 @@ public class CorporateEntity extends BaseEntity {
     @Column(name = "identity", unique = true, nullable = false, length = 25)
     private String identity;
 
-    @Column(name = "password", unique = true, nullable = false, length = 100)
+    @Column(name = "password", nullable = false, length = 100)
     private String password;
 
     @Column(name = "name", nullable = false, length = 25)
--- src/main/java/kr/co/popoolserver/user/domain/UserEntity.java
+++ src/main/java/kr/co/popoolserver/user/domain/UserEntity.java
@@ -22,7 +22,7 @@ public class UserEntity extends BaseEntity {
     @Column(name = "identity", unique = true, nullable = false, length = 25)
     private String identity;
 
-    @Column(name = "password", unique = true, nullable = false, length = 100)
+    @Column(name = "password", nullable = false, length = 100)
     private String password;
 
     @Column(name = "email", unique = true, length = 50)
