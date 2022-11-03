b074dbf5171b28eca3b6f962a2ee07096943b691
홍혁준 <jjj8337721@naver.com>
2022-11-03 12:54:52 +0900

    feat: CRUD를 위한 Exception Response 구성

---
+++ src/main/java/kr/co/popoolserver/common/infra/error/model/ErrorCode.java
@@ -0,0 +1,23 @@
+package kr.co.popoolserver.common.infra.error.model;
+
+import lombok.Getter;
+
+@Getter
+public enum ErrorCode {
+
+    //TODO: common, corporate, payment, product, inventory, career, score, grade Error Enum
+
+    //user
+    WRONG_PASSWORD("비밀번호를 다시 확인해주세요", 400),
+    DUPLICATED_ID("중복된 아이디를 사용할 수 없습니다.", 400),
+    DUPLICATED_PHONE("중복된 전화번호를 사용할 수 없습니다.", 400),
+    DUPLICATED_EMAIL("중복된 이메일을 사용할 수 없습니다.", 400);
+
+    private String message;
+    private int status;
+
+    ErrorCode(String message, int status) {
+        this.message = message;
+        this.status = status;
+    }
+}
