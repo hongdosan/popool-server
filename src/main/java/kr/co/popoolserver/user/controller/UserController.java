62162702d9d18864b424024428e2addf4b4e2f70
홍혁준 <jjj8337721@naver.com>
2022-11-09 18:03:39 +0900

    feat : 공통 회원 Controller 분리

--- src/main/java/kr/co/popoolserver/user/controller/UserController.java
+++ src/main/java/kr/co/popoolserver/user/controller/UserController.java
@@ -1,13 +1,9 @@
 package kr.co.popoolserver.user.controller;
 
 import io.swagger.annotations.ApiOperation;
-import kr.co.popoolserver.common.domain.enums.ServiceName;
 import kr.co.popoolserver.common.infra.error.model.ResponseFormat;
-import kr.co.popoolserver.user.domain.dto.UserCommonDto;
 import kr.co.popoolserver.user.domain.dto.UserDto;
-import kr.co.popoolserver.user.domain.service.UserCommonService;
 import kr.co.popoolserver.user.domain.service.UserService;
-import kr.co.popoolserver.user.domain.service.impl.UserCommonServiceProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,15 +15,6 @@ import javax.validation.Valid;
 public class UserController {
 
     private final UserService userService;
-    private final UserCommonServiceProvider userCommonServiceProvider;
-
-    @ApiOperation("Login")
-    @PostMapping("/{serviceName}/login")
-    public ResponseFormat<UserCommonDto.TOKEN> login(@PathVariable ServiceName serviceName,
-                                                     @RequestBody @Valid UserCommonDto.LOGIN login){
-        UserCommonService userCommonService = userCommonServiceProvider.getUserService(serviceName);
-        return ResponseFormat.ok(userCommonService.login(login));
-    }
 
     @ApiOperation("일반 회원 회원가입")
     @PostMapping("/signUp")
@@ -40,34 +27,6 @@ public class UserController {
     @PutMapping
     public ResponseFormat updateUser(@RequestBody @Valid UserDto.UPDATE update){
         userService.updateUser(update);
-        return ResponseFormat.ok();
-    }
-
-    @ApiOperation("일반 회원 비밀번호 변경")
-    @PutMapping("/password")
-    public ResponseFormat updateUserPassword(@RequestBody @Valid UserCommonDto.UPDATE_PASSWORD password){
-        userService.updatePassword(password);
-        return ResponseFormat.ok();
-    }
-
-    @ApiOperation("일반 회원 메일 변경")
-    @PutMapping("/email")
-    public ResponseFormat updateUserEmail(@RequestBody @Valid UserCommonDto.UPDATE_EMAIL email){
-        userService.updateEmail(email);
-        return ResponseFormat.ok();
-    }
-
-    @ApiOperation("일반 회원 전화번호 변경")
-    @PutMapping("/phone")
-    public ResponseFormat updateUserPhone(@RequestBody @Valid UserCommonDto.UPDATE_PHONE phone){
-        userService.updatePhone(phone);
-        return ResponseFormat.ok();
-    }
-
-    @ApiOperation("일반 회원 주소 변경")
-    @PutMapping("/address")
-    public ResponseFormat updateUserAddress(@RequestBody @Valid UserCommonDto.UPDATE_ADDRESS address){
-        userService.updateAddress(address);
         return ResponseFormat.ok();
     }
 
