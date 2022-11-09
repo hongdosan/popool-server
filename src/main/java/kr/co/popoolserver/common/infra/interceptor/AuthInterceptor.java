9a757f5ae551041b18deb343cd5c5137307e7915
홍혁준 <jjj8337721@naver.com>
2022-11-09 11:16:25 +0900

    fix

--- src/main/java/kr/co/popoolserver/common/infra/interceptor/AuthInterceptor.java
+++ src/main/java/kr/co/popoolserver/common/infra/interceptor/AuthInterceptor.java
@@ -14,7 +14,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.constraints.NotNull;
 
-@RequiredArgsConstructor
 public class AuthInterceptor implements HandlerInterceptor {
 
     @Autowired
