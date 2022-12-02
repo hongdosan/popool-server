e63c504f42a02c28b8e96ab101b7712cbb50c57e
홍혁준 <jjj8337721@naver.com>
2022-12-02 22:27:14 +0900

    feat: redis Service 1) get 2) create 3) delete 4) check

--- src/main/java/kr/co/popoolserver/user/domain/service/impl/RedisServiceImpl.java
+++ src/main/java/kr/co/popoolserver/user/domain/service/impl/RedisServiceImpl.java
@@ -1,9 +1,14 @@
 package kr.co.popoolserver.user.domain.service.impl;
 
+import kr.co.popoolserver.common.infra.error.exception.JwtTokenExpiredException;
+import kr.co.popoolserver.common.infra.error.model.ErrorCode;
 import kr.co.popoolserver.user.domain.service.RedisService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
 
 @Service
 @RequiredArgsConstructor
@@ -13,22 +18,22 @@ public class RedisServiceImpl implements RedisService {
 
     @Override
     public String getValue(String key) {
-        //TODO Get Value
-        return null;
+        ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
+        return valueOperations.get(key);
     }
 
     @Override
     public void createData(String key, String value, long expired) {
-        //TODO Create Data
+        redisTemplate.opsForValue().set(key, value, expired, TimeUnit.MILLISECONDS);
     }
 
     @Override
     public void deleteData(String key) {
-        //TODO Delete Data
+        redisTemplate.delete(key);
     }
 
     @Override
     public void checkValue(String refreshToken, String redisToken) {
-        //TODO Check Value
+        if(!redisToken.equals(redisToken)) throw new JwtTokenExpiredException(ErrorCode.FAIL_EXPIRE);
     }
 }
