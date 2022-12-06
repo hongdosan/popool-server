6901e71a20bb3265bcd25c0cc8319101cbbc2226
홍혁준 <jjj8337721@naver.com>
2022-12-06 15:40:43 +0900

    fix: Career FIle API

--- src/main/java/kr/co/popoolserver/career/controller/CareerFileController.java
+++ src/main/java/kr/co/popoolserver/career/controller/CareerFileController.java
@@ -1,5 +1,6 @@
 package kr.co.popoolserver.career.controller;
 
+import io.swagger.annotations.ApiOperation;
 import kr.co.popoolserver.career.domain.dto.CareerFileDto;
 import kr.co.popoolserver.career.domain.service.CareerFileService;
 import kr.co.popoolserver.common.infra.error.model.ResponseFormat;
@@ -13,15 +14,38 @@ public class CareerFileController {
 
     private final CareerFileService careerFileService;
 
+    @ApiOperation("CareerFile 생성")
+    @PostMapping
+    public ResponseFormat createCareerFile(){
+        //TODO Create Career File;
+        return ResponseFormat.ok();
+    }
+
+    @ApiOperation("CareerFile 수정")
+    @PutMapping
+    public ResponseFormat updateCareerFile(){
+        //TODO Update Career File;
+        return ResponseFormat.ok();
+    }
+
+    @ApiOperation("CareerFile Info 읽기")
     @GetMapping("/info")
     public ResponseFormat<CareerFileDto.READ_INFO> getCareerFileInfo() {
+        //TODO IMAGE INFO READ
         return ResponseFormat.ok(careerFileService.getCareerFileInfo());
     }
 
+    @ApiOperation("CareerFile Image 읽기")
     @GetMapping
     public ResponseFormat getCareerFile() {
         //TODO IMAGE READ
         return ResponseFormat.ok();
     }
 
+    @ApiOperation("CareerFile 삭제")
+    @DeleteMapping
+    public ResponseFormat deleteCareerFile(){
+        //TODO Delete Career File;
+        return ResponseFormat.ok();
+    }
 }
