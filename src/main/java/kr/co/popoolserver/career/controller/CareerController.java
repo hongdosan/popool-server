84e0eb4ae08587d6184a4d3d6f239f4365c3483b
홍혁준 <jjj8337721@naver.com>
2022-12-07 15:31:09 +0900

    fix : Career API

--- src/main/java/kr/co/popoolserver/career/controller/CareerController.java
+++ src/main/java/kr/co/popoolserver/career/controller/CareerController.java
@@ -1,5 +1,6 @@
 package kr.co.popoolserver.career.controller;
 
+import io.swagger.annotations.ApiOperation;
 import kr.co.popoolserver.career.domain.dto.CareerDto;
 import kr.co.popoolserver.career.domain.service.CareerService;
 import kr.co.popoolserver.common.infra.error.model.ResponseFormat;
@@ -15,28 +16,33 @@ public class CareerController {
 
     private final CareerService careerService;
 
+    @ApiOperation("이력서 생성")
     @PostMapping("/create")
     public ResponseFormat createCareer(@RequestBody CareerDto.CREATE create) {
         careerService.createCareer(create);
         return ResponseFormat.ok();
     }
 
+    @ApiOperation("나의 모든 이력서 조회")
     @GetMapping
     public ResponseFormat<List<CareerDto.READ>> getAllCareers() {
         return ResponseFormat.ok(careerService.getAllCareers());
     }
 
+    @ApiOperation("나의 이력서 1개 조회")
     @GetMapping("/one")
     public ResponseFormat getCareer(@RequestParam("career_id") Long careerId) {
         return ResponseFormat.ok(careerService.getCareer(careerId));
     }
 
+    @ApiOperation("이력서 수정")
     @PutMapping
     public ResponseFormat updateCareer(@RequestBody CareerDto.UPDATE update) {
         careerService.updateCareer(update);
         return ResponseFormat.ok();
     }
 
+    @ApiOperation("이력서 삭제")
     @DeleteMapping
     public ResponseFormat deleteCareer(@RequestParam("career_id") Long careerId) {
         careerService.deleteCareer(careerId);
