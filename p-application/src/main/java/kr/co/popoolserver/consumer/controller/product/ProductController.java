e28a7680675a0b33fdae967846c1f49db5b83d33
홍혁준 <jjj8337721@naver.com>
2022-12-23 16:43:07 +0900

    feat : Product Name All Get Service

--- p-application/src/main/java/kr/co/popoolserver/consumer/controller/product/ProductController.java
+++ p-application/src/main/java/kr/co/popoolserver/consumer/controller/product/ProductController.java
@@ -19,8 +19,7 @@ public class ProductController {
     @ApiOperation("모든 상품 조회 / 권한 : 일반 회원 이상")
     @GetMapping
     public ResponseFormat<List<ProductDto.READ>> getAllProducts(){
-        ///TODO get all Products
-        return ResponseFormat.ok();
+        return ResponseFormat.ok(productService.getProducts());
     }
 
     @ApiOperation("상품 세부사항 조회 / 권한 : 일반 회원 이상")
