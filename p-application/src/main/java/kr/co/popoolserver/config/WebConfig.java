fe41b7fa2dda9b07d0fd409dbaa710f3cbe2e36b
홍혁준 <jjj8337721@naver.com>
2023-01-09 23:34:29 +0900

    fix : github action yaml file workflow update and dockerfile create

--- p-application/src/main/java/kr/co/popoolserver/config/WebConfig.java
+++ p-application/src/main/java/kr/co/popoolserver/config/WebConfig.java
@@ -3,11 +3,15 @@ package kr.co.popoolserver.config;
 import kr.co.popoolserver.interceptor.AuthInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    private static final long MAX_AGE_SECOND = 3600;
+
     private static final String[] AUTH_ARR = {
             "/swagger/**",
             "/v2/api-docs",
@@ -50,6 +54,16 @@ public class WebConfig implements WebMvcConfigurer {
                 .excludePathPatterns(USER_AUTH_ARR)
                 .excludePathPatterns(PRODUCT_AUTH_ARR)
                 .excludePathPatterns(AUTH_ARR);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(MAX_AGE_SECOND);
     }
 
     @Bean
